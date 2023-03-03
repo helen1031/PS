212e401987d63c4864dd0c7945eb89fc241bc011
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-03 15:14:18 +0900

    [Silver IV] Title: 카드 놓기, Time: 44 ms, Memory: 32792 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,23 +1,10 @@
+from itertools import permutations
+
 n = int(input())
 k = int(input())
 cards = [input() for _ in range(n)]
-used = [False for _ in range(n)]
-select = []
-choices = []
-
-def rec(idx):
-    if len(select) == k:
-        choices.append(''.join(select))
-        return
-    for i in range(n):
-        if used[i]:
-            continue
-        select.append(cards[i])
-        used[i] = True
-        rec(i+1)
-        select.pop()
-        used[i] = False
-        
-    
-rec(0)
-print(len(set(choices)))
+cases = list(permutations(cards, k))
+res = []
+for case in cases:
+    res.append(''.join(case))
+print(len(set(res)))
