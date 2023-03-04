8862f49fe3dc277d8464c8fb5b7c79444296492b
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-04 16:05:20 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 0
+for i in range(1, n+1):
+    if i < 100 :
+        cnt += 1
+    else:
+        nums = list(map(int, str(i)))
+        if nums[0] - nums[1] == nums[1] - nums[2]:
+            cnt += 1
+            
+print(cnt)
