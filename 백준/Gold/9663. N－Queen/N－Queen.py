3b0552a12ae60debc996d70a0c4a7a9be723c87b
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-04 11:07:52 +0900

    [Gold IV] Title: N-Queen, Time: 23376 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,27 +1,21 @@
 n = int(input())
+info = [0] * n
+located = [False] * n
+diag1 = [False] * (2 * n-1)
+diag2 = [False] * (2 * n-1)
 ans = 0
-located = [0] * n
 
-def check(y, x):
-    # 놓인 열이 같거나 대각선에 있으면(기울기가 -1 or 1) False를 반환한다
-    for j in range(y): # j는 행
-        i = located[j] # i는 열
-        if i == x or abs(i - x) == y - j:
-            return False
-    return True
-
-def dfs(y):
+def set(i):
     global ans
-    if y == n:
-        ans += 1
-        return
-    # cnt번째 행의 각 열을 돌면서(=각 행마다 퀸이 하나씩 놓여야 한다)
-    # 놓을 수 있는 곳인지 검사한다
-    # 놓을 수 있다면 dfs
-    for x in range(n):
-        located[y] = x # 열
-        if check(y, x):
-            dfs(y + 1)
-
-dfs(0)
+    for j in range(n):
+        if not located[j] and not diag1[i+j] and not diag2[i-j+ n -1]:
+            info[i] = j
+            if i == n - 1:
+                ans += 1
+            else:
+                located[j] = diag1[i+j] = diag2[i-j+ n -1] = True
+                set(i+1)
+                located[j] = diag1[i+j] = diag2[i-j+ n -1] = False
+    
+set(0)
 print(ans)
