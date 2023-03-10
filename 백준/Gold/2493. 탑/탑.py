f887819c7b344ef4e2de7d12c2c946e6b35b9e26
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-10 15:33:51 +0900

    [Gold V] Title: 탑, Time: 816 ms, Memory: 137288 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+towers = list(map(int, input().split()))
+stack = []
+answer = [0 for i in range(n)]
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] > towers[i]:
+            answer[i] = stack[-1][0] + 1
+            break
+        else:
+            stack.pop()
+    stack.append([i, towers[i]])
+
+print(*answer)
