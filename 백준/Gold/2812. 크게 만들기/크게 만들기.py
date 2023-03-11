cffa33d58ef800930f63303ba75b9f6a85288d49
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-11 16:26:52 +0900

    [Gold III] Title: 크게 만들기, Time: 176 ms, Memory: 37392 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -3,18 +3,20 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 nums = list(input().rstrip())
+
 st = []
 cnt = 0
 for num in nums:
     while st and cnt < k:
-        if st[-1] < num:
+        if st[-1] >= num:
+            break
+        else:
             st.pop()
             cnt += 1
-        else:
-            break
     st.append(num)
-    
-if len(st) > n - k:
+            
+if cnt < k:
     st = st[:n-k]
-    
+
 print(''.join(st))
+    
