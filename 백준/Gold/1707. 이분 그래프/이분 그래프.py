e771a95556a1391f8f27ccd1aa8cdc8f29c27119
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-17 09:07:00 +0900

    [Gold IV] Title: 이분 그래프, Time: 1204 ms, Memory: 53200 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,41 +1,37 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     v, e = map(int, input().split())
     graph = [[] for _ in range(v+1)]
-    
     for _ in range(e):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
-        
-    visited = [False] * (v+1)
+    visited = [False] * (v + 1)
     cycle = False
     
-    def dfs(cur, group):
+    def dfs(x, prv):
         global cycle
         
-        if cycle:
+        if cycle :
             return
         
-        visited[cur] = group
-        
-        for x in graph[cur]:
-            if not visited[x]:
-                dfs(x, -group)
-            elif visited[cur] == visited[x]:
+        visited[x] = prv
+        for i in graph[x]:
+            if not visited[i]:
+                dfs(i, -prv)    #그래프의 정점을 두가지 색으로 칠할 때, 인접한 정점끼리는 다른 색을 가지고 있는 그래프
+            elif visited[x] == visited[i]:
                 cycle = True
                 return
-                
-        
-    for i in range(1, v+1):
+    
+    for i in range(1, v + 1):
         if not visited[i]:
             dfs(i, 1)
             if cycle:
                 break
-    
+                
     if cycle:
         print("NO")
     else:
