2b2dccd5745372cbac9add20ca4fb4be3a43a432
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-21 10:26:16 +0900

    [Gold IV] Title: 사이클 게임, Time: 776 ms, Memory: 50684 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+cycle = False
+for i in range(1, m + 1):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print(i)
+else:
+    print(0)
