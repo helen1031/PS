156a9159a182052308b35c074b7a8a09be0f896c
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-23 10:52:30 +0900

    [Gold II] Title: 트리, Time: 780 ms, Memory: 31412 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.py
+++ 백준/Gold/4256. 트리/트리.py
@@ -1,26 +1,25 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
 
 def toPost(p_s, p_e, i_s, i_e):
-    global postorder
-    
+
     if p_s <= p_e and i_s <= i_e:
         root = preorder[p_s]
-        idx = inorder.index(root)
-        
-        toPost(p_s + 1, p_s + idx - i_s, i_s, idx - 1 )
-        toPost(p_s + idx - i_s + 1, p_e, idx + 1, i_e)
+        ridx = inorder.index(root)
+
+        # 왼쪽
+        toPost(p_s + 1, p_s + ridx - i_s, i_s, ridx - 1)
+        # 오른쪽
+        toPost(p_s + ridx - i_s + 1, p_e, ridx + 1, i_e)
+        # 루트
         postorder.append(root)
-    
-    else:
-        return
-    
+
 for _ in range(int(input())):
     n = int(input())
     preorder = list(map(int, input().split()))
     inorder = list(map(int, input().split()))
     postorder = []
+
     toPost(0, n-1, 0, n-1)
-    
+
     print(*postorder)
