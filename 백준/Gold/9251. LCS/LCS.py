46d49d3f825ef09131ecd7402133598c23e40c61
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-24 15:00:57 +0900

    [Gold V] Title: LCS, Time: 524 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,30 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-a = input().strip()
-b = input().strip()
+x = ' ' + input().rstrip()
+y = ' ' + input().rstrip()
 
-dp = [[0] * (len(a)) for _ in range(len(b))]
-if a[0] == b[0]:
-    dp[0][0] = 1
-    
-for x in range(1, len(a)):
-    dp[0][x] = dp[0][x-1]
-    if a[x] == b[0]:
-        dp[0][x] = 1
-        
-for y in range(1, len(b)):
-    dp[y][0] = dp[y-1][0]
-    if b[y] == a[0]:
-        dp[y][0] = 1
-        
-        
-for y in range(1, len(b)):
-    for x in range(1, len(a)):
-        dp[y][x] = dp[y-1][x-1]
-        if b[y] == a[x]:
-            dp[y][x] += 1
-        dp[y][x] = max(dp[y][x], dp[y-1][x], dp[y][x-1])
+dp = [[0] * len(y) for _ in range(len(x))]
+
+for i in range(1, len(x)):
+    for j in range(1, len(y)):
+        if x[i] == y[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
             
-print(dp[len(b)-1][len(a)-1])
-        
+print(dp[-1][-1])
