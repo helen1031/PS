703b6fb4a8c5b55849ed1a8467da1dec481924ff
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-29 09:45:08 +0900

    [Gold I] Title: 외판원 순회, Time: 996 ms, Memory: 46676 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -5,12 +5,10 @@ n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 ALL_VISIT = (1 << n) - 1
 
-#dp[n][visited] : N번 -> visited에서 방문 X한 도시 -> 0번 도시(시작 도시) 경로 저장
 dp = [[None] * (1 << n) for _ in range(n)]
 INF = int(1e9)
-idx = 1
 
-def travel(last, visited):
+def dfs(last, visited):
     if visited == ALL_VISIT:
         return graph[last][0] or INF
     
@@ -20,8 +18,8 @@ def travel(last, visited):
     tmp = INF
     for city in range(n):
         if visited & (1 << city) == 0 and graph[last][city] != 0:
-            tmp = min(tmp, travel(city, visited | (1 << city)) + graph[last][city])
+            tmp = min(tmp, dfs(city, visited | (1 << city)) + graph[last][city])
     dp[last][visited] = tmp
-    return tmp
-
-print(travel(0, 1 << 0))
+    return dp[last][visited]
+    
+print(dfs(0, 1 << 0))
