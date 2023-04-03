657ccd830b80cf68d651c6e2db2e896646fc88f0
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-04-03 09:01:52 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,18 +1,21 @@
-n = int(input())
-schedule = []
-for i in range(n):
-    schedule.append(list(map(int, input().split())))
+import sys
+input = sys.stdin.readline
 
-dp = [0] * (n+1)
-maxp = 0
-#일자역순
-for i in range(n-1, -1, -1):
-    t = i + schedule[i][0] # 상담 마치는 시점
-    # 상담 종료일이 기간 내
-    if t <= n:
-        dp[i] = max(schedule[i][1] + dp[t], maxp)   #오늘 상담 가격 + 상담종료일 이후 얻는 금액
-        maxp = dp[i]
-    # 기간 외
+n = int(input())
+information = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (n + 1)
+mcost = 0
+
+# 날짜 역순 탐색
+for d in range(n-1, -1, -1):
+    day, cost = information[d]
+    # 퇴사일 초과
+    if d + day > n:
+        dp[d] = mcost
+    # 퇴사일 이내
     else:
-        dp[i] = maxp
-print(maxp)
+        dp[d] = max(dp[d + day] + cost, mcost)
+        mcost = dp[d]
+        
+print(dp[0])
