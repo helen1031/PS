87aa67c9d2aae6781122ca700196ae084879ba22
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-04-04 09:03:31 +0900

    [Silver III] Title: 등수 매기기, Time: 464 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -5,10 +5,11 @@ n = int(input())
 predicts = [int(input()) for _ in range(n)]
 predicts.sort()
 
-dislike = 0
+dislikes = 0
+
 rank = 1
-for i, predict in enumerate(predicts):
-    dislike += abs(predict - rank)
+for predict in predicts:
+    dislikes += abs(predict - rank)
     rank += 1
 
-print(dislike)
+print(dislikes)
