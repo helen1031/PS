fe12defa9ca25bd88a855a042cc3ec6b534cf30a
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-04-11 09:10:21 +0900

    [Gold IV] Title: 부분합, Time: 120 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,25 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
+part = nums[0]
+ans = INF
 
-ans = 100000001
-left = 0
-right = 0
-partial = 0
-length = 0
+left, right = 0, 0
 
 while True:
-    if partial >= s:
-        ans = min(ans, length)
-        partial -= nums[left]
+    if part >= s:
+        ans = min(ans, right - left + 1)
+        part -= nums[left]
         left += 1
-        length -= 1
-    elif right == n:
+
+    elif right == n -1:
         break
+
     else:
-        partial += nums[right]
         right += 1
-        length += 1
-    
-if ans == 100000001:
-    ans = 0
-print(ans)
+        part += nums[right]
+
+print(0) if ans == INF else print(ans)
